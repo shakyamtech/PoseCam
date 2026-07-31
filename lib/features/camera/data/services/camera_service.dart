e62d6f8ae1101757b073a4f8fb85bfd6b174249c
Your Nameshakyamtech <shakya.mahes@gmail.com>
2026-07-31 18:53:22 +0545

    fix(camera): add missing Offset import in camera_service.dart for web compilation

--- lib/features/camera/data/services/camera_service.dart
+++ lib/features/camera/data/services/camera_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import '../../domain/models/camera_state.dart';
 
